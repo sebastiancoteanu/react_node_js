a5bf5b916683e6fb48742266b9ee8c8573e1095c
Sebastian Coteanu <sebastiancoteanu@gmail.com>
2018-11-04 23:21:42 +0200

    Add passport.js. Start integration.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
-
-app.get('/', (req, res) => {
-	res.send({ anotherOne: 'sebi'});
-});
-
 const PORT = process.env.PORT || 5000;
+
+passport.use(new GoogleStrategy())
+
 app.listen(PORT);

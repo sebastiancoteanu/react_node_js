847708e47cfdfad3d0440a34a99ca53d7d8b753c
Sebastian Coteanu <sebastian.coteanu@gmail.com>
2018-12-20 17:43:06 +0200

    Add stripe button

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,3 @@ ReactDOM.render(
     <Provider store={store}><App /></Provider>,
     document.querySelector('#root')
 );
-
-console.log('stripe key is', process.env.REACT_APP_STRIPE_KEY);
-console.log('env is', process.env.NODE_ENV); 
